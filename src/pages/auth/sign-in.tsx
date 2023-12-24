a19ca223639b161c04ab445d1e1e555b9c11d7fc
Fernando D'luccas <dluccash@hotmail.com>
2023-12-24 14:50:37 -0300

    feat: integrate React Helmet for SEO

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,11 @@
+import { Helmet } from 'react-helmet-async'
+
 import { Button } from '@/components/ui/button'
 
 export function SignIn() {
   return (
     <div className="flex h-screen items-center justify-center">
+      <Helmet title="Autenticação" />
       <Button>Click me</Button>
     </div>
   )
