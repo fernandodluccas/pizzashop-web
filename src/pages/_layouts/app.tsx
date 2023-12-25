812c7329f2443afcf3c22184b2609f6cd6e25f7d
Fernando D'luccas <dluccash@hotmail.com>
2023-12-25 00:25:06 -0300

    feat: create app layout with header

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+
+import { Header } from '@/components/header'
 
 export function AppLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground"></div>
-      <Outlet />
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
     </div>
   )
 }
